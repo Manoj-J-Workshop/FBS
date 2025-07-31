197f3000dd0e0d71f354565b9629d9e188666f86
Manoj J <manojj91298@gmail.com>
2025-07-31 05:40:30 +0530

    8th commit - Working on Authentication services

---
+++ central-api/src/main/java/com/fbs/central_api/utility/AuthUtility.java
@@ -0,0 +1,32 @@
+package com.fbs.central_api.utility;
+
+import io.jsonwebtoken.Jwts; // This will help to build JWT Token
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class AuthUtility {
+
+    @Value("${jwt.expiration.time}")
+    long  expirationTime;
+
+    @Value("${jwt.secret.password}")
+    String secretPassword;
+    // JWT Token -> Payload, Header, Signature (Secure Password + Algorithm)]
+    // JWT Token -> We are encrypting the data with the help of Algorithm + SecretPassword
+    public String generateToken(String email,
+                                String password,
+                                String role){
+        String payload = email + ":" + password + ":" + role;
+        String jwtToken = Jwts.builder()
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, secretPassword)
+                .setSubject(payload)
+                .compact();
+        return jwtToken;
+    }
+}
